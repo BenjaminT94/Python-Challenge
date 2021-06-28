96fb0e3f10b7004b06e149ba75933e1716ef7d5a
Benjamin Tran <Benjietran1994@gmail.com>
2021-06-28 15:29:15 -0700

    Minor Edit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,6 @@
 import os
 import csv
-file = os.path.join('..', 'Resources','budget_data.csv')
+file = os.path.join('budget_data.csv')
 # Using Exercise CSV Read, opening the source data file
 with open('budget_data.csv','r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
@@ -18,7 +18,7 @@ with open('budget_data.csv','r') as csvfile:
         profit_change.append(profit_change[x+1]-profit_change[x])
         # Assigning a variable to calculate the average change of profit in the entire file  
         average_change = sum(profit_change) / len(profit_change) 
-        # Tracking the index of the change in profit each month plus 1 (House of Pie exercise)
+        # Tracking the index of the greatest change in profit plus 1 (House of Pie exercise)
         greatest_increase = profit_change.index(max(profit_change)) + 1
         greatest_decrease = profit_change.index(min(profit_change)) + 1
 
