9ace56fd1356ca7fcaf801613f993d2b5800bf05
Benjamin Tran <Benjietran1994@gmail.com>
2021-06-28 00:22:00 -0700

    Edited

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,6 @@
 import os
 import csv
-data = os.path.join('budget_data.csv')
+file = os.path.join('budget_data.csv')
 # Using Exercise CSV Read, opening the source data file
 with open('budget_data.csv','r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
@@ -10,17 +10,18 @@ with open('budget_data.csv','r') as csvfile:
     months = []
     profit_loss = []
     profit_change = []
-    # Assigning a variable to calculate the average change of profit in the entire file  
-    average_change = sum(profit_change) / len(profit_change) 
-    # Tracking the index of the change in profit each month plus 1 (House of Pie exercise)
-    greatest_increase = profit_change.index(max(profit_change)) + 1
-    greatest_decrease = profit_change.index(min(profit_change)) + 1
-# Using a for loop to now append values to our lists
-    for x in range(len(profit_change)-1):
-        profit_change.append(profit_change[x+1]-profit_change[x])
+# Using a for loop to now append values to our list
     for row in csvreader:
         months.append(row[0])
         profit_loss.append(int(row[1]))
+    for x in range(len(profit_change)-1):
+        profit_change.append(profit_change[x+1]-profit_change[x])
+        # Assigning a variable to calculate the average change of profit in the entire file  
+        average_change = sum(profit_change) / len(profit_change) 
+        # Tracking the index of the change in profit each month plus 1 (House of Pie exercise)
+        greatest_increase = profit_change.index(max(profit_change)) + 1
+        greatest_decrease = profit_change.index(min(profit_change)) + 1
+
 print("Financial Analysis")
 print("--------------------")
 print(f"Total Months: {len(months)}")
