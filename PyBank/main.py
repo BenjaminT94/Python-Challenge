8f75b83292272bdfb2056d2567f2943f1f1e3fcf
Benjamin Tran <Benjietran1994@gmail.com>
2021-06-27 18:43:43 -0700

    Assigning variables

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,3 +6,12 @@ with open('budget_data.csv','r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
 # Skipping the first row as it's the row for column headers
     header = next(csvreader)
+# Creating lists to track values for the output
+    months = []
+    profit_loss = []
+    profit_change = []
+# Assigning a variable to calculate the average change of profit in the entire file
+    average_change = sum(profit_change) / len(profit_change)
+# Tracking the index of the change in profit each month plus 1
+    increase = profit_change.index(max(profit_change)) + 1
+    decrease = profit_change.index(min(profit_change)) + 1
