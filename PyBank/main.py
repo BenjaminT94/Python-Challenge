01e93897ba88583d5080e3893be2cbc4786a7431
Benjamin Tran <Benjietran1994@gmail.com>
2021-06-28 13:21:45 -0700

    Resources folder

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,9 @@
 import os
 import csv
-file = os.path.join('budget_data.csv')
+file = os.path.join("..", "Resources","budget_data.csv")
 # Using Exercise CSV Read, opening the source data file
-with open('budget_data.csv','r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
+with open("budget_data.csv","r") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
 # Skipping the first row as it's the row for column headers
     header = next(csvreader)
 # Creating lists to track values for the output
@@ -14,7 +14,7 @@ with open('budget_data.csv','r') as csvfile:
     for row in csvreader:
         months.append(row[0])
         profit_loss.append(int(row[1]))
-    for x in range(len(profit_change)-1):
+    for x in range(len(profit_loss)-1):
         profit_change.append(profit_change[x+1]-profit_change[x])
         # Assigning a variable to calculate the average change of profit in the entire file  
         average_change = sum(profit_change) / len(profit_change) 
@@ -29,6 +29,5 @@ print(f"Total: {sum(profit_loss)}")
 print(f"Average Change: {int(average_change)}")
 print(f"Greatest Increase: {months[greatest_increase]} {(str(max(profit_change)))}")
 print(f"Greatest Decrease: {months[greatest_decrease]} {(str(min(profit_change)))}")
-newfile = os.path.join('new.txt')
-with open(newfile,"w") as new:
-    
+
+
